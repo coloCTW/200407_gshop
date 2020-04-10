9ab86f54f51d97414e384f861d708be0ce6bd129
coloCTW <903255779@qq.com>
2020-04-10 18:28:59 +0800

    103

--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -12,20 +12,21 @@
     </HeaderTop>
     <!--首页导航-->
     <nav class="msite_nav">
-      <div class="swiper-container">
+      <div class="swiper-container" v-if="categorys.length">
         <div class="swiper-wrapper">
-          <div class="swiper-slide" v-for="(categorys, index)in categorysArr" :key="index">
-            <a href="javascript:" class="link_to_food" v-for="(category, index)in categorys" :key="index">
+          <div class="swiper-slide" v-for="(categorys, index) in categorysArr" :key="index">
+            <a href="javascript:" class="link_to_food" v-for="(category, index) in categorys" :key="index">
               <div class="food_container">
-                <img src="./images/nav/1.jpg">
+                <img :src="baseImgUrl+category.image_url">
               </div>
-              <span>甜品饮品</span>
+              <span>{{category.title}}</span>
             </a>
           </div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
       </div>
+      <img src="./images/msite_back.svg" alt="back" v-else>
     </nav>
     <!--首页附近商家-->
     <div class="msite_shop_list">
@@ -48,20 +49,59 @@
 
   import {mapState} from 'vuex'
   export default{
+    data(){
+      return{
+          baseImgUrl:'https://fuss10.elemecdn.com'
+      }
+    },
     mounted(){
-      //创建一个Swiper实例对象，来实现轮播
-      new Swiper('.swiper-container',{
-        loop: true, // 循环模式选项
-
-        // 如果需要分页器
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      })
+      this.$store.dispatch('getCategorys')
+      this.$store.dispatch('getShops')
     },
 
     computed:{
-      ...mapState(['address'])
+      ...mapState(['address','categorys']),
+
+      /*
+      * 根据categorys一维数组生成一个二维数组
+      * 小数组中的元素个数最大是8
+      */
+      categorysArr(){
+        const {categorys} = this
+        //准备空的二维数组
+        const arr = []
+        //准备一个小数组，最大长度为8
+        let minArr = []
+        //遍历categorys
+        categorys.forEach(c => {
+          //如果小数组长度为8，则清空小数组
+          if(minArr.length===8){
+              minArr=[]
+          }
+          //如果小数组是空的，加入到二维数组中
+          if(minArr.length===0){
+            arr.push(minArr)
+          }
+            minArr.push(c)
+        })
+        return arr
+      }
+    },
+
+    watch:{
+      categorys(value){ //categorys有数据了，但是还没有更新界面
+        this.$nextTick(()=>{//一旦更新界面，就会调用这条语句，一定要写在数据更新之后
+          //创建一个Swiper实例对象，来实现轮播
+          new Swiper('.swiper-container',{
+            loop: true, // 循环模式选项
+
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          })
+        })
+      }
     },
 
     components:{
